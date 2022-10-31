9489892e9abcf68364f8b8311d120ccba561e115
lucascincunegui <79164570+lucascincunegui@users.noreply.github.com>
2022-10-30 23:50:21 -0300

    add grid in products

--- src/components/card.js
+++ src/components/card.js
@@ -3,12 +3,23 @@ import { Card, CardContent, CardMedia, Typography } from "@material-ui/core";
 
 export default function ProductCard({ name, value, image }) {
   return (
-    <Card>
+    <Card
+      style={{
+        textAlign: "center",
+        minWidth: "350px",
+        margin: "1em",
+        boxSizing: "border-box",
+      }}
+    >
       <CardContent>
         <Typography>{name}</Typography>
         <Typography>{value}</Typography>
       </CardContent>
-      <CardMedia alt="imagen" src={image} />
+      <CardMedia
+        style={{ minHeight: 300, margin: "1em" }}
+        alt="imagen"
+        image={image}
+      />
     </Card>
   );
 }
--- src/components/list.js
+++ src/components/list.js
@@ -1,29 +1,41 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
 import ProductCard from "../components/card";
-import { DivLista } from "../components/constants";
 
 export default function Lista() {
   const productsArray = [
-    { image: "/assets/golden.jpeg", value: "R$ 140,00", name: "Golden" },
-
-    { image: "/assets/sapeca.jpeg", value: "R$ 140,00", name: "Sapeca" },
+    {
+      image: "/assets/racoes/golden.jpeg",
+      value: "R$ 122,00",
+      name: "Golden",
+    },
 
     {
-      image: "/assets/pastel dog.jpeg",
+      image: "/assets/racoes/sapeca.jpeg",
+      value: "R$ 100,00",
+      name: "Sapeca",
+    },
+
+    {
+      image: "/assets/racoes/pastel dog.jpeg",
       value: "R$ 140,00",
       name: "Pastel Dog",
     },
 
-    { image: "/assets/premier.jpeg", value: "R$ 140,00", name: "Premier" },
-
-    { image: "/assets/bravo.jpeg", value: "R$ 140,00", name: "Bravo" },
+    {
+      image: "/assets/racoes/premier.jpeg",
+      value: "R$ 140,00",
+      name: "Premier",
+    },
   ];
-  console.log(productsArray);
+
   return (
-    <DivLista>
+    <Grid container spacing={3} justifyContent="center">
       {productsArray.map((card, index) => {
-        return <ProductCard name={card.name} />;
+        return (
+          <ProductCard name={card.name} value={card.value} image={card.image} />
+        );
       })}
-    </DivLista>
+    </Grid>
   );
 }
