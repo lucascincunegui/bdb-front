2c682db3b95efc0bb45a59db5889e1394d0b3c82
lucascincunegui <79164570+lucascincunegui@users.noreply.github.com>
2022-11-22 23:50:25 -0300

    header_1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,32 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {
-  Footer,
-  Header,
-  SubHeader,
-  Title,
-  Wrapper,
-} from "../components/constants";
+import { Footer, Title, Wrapper } from "../components/constants";
+import { Header, SubHeader } from "./styles";
 import NavLinks from "../components/navLinks";
 import Home from "../pages/home";
 import Products from "../pages/products";
 import Error404 from "../pages/error404";
+import { Paper } from "@material-ui/core";
 
 export default function MyRoutes() {
   return (
     <Wrapper>
       <Router>
-        <Header>
-          <SubHeader>
-            <Title>Bulichu Dos Bichus</Title>
-            <NavLinks />
-          </SubHeader>
-          <img
-            style={{ border: "solid 1px rgb(254, 210, 39)" }}
-            width={450}
-            src="/assets/pagina.png"
-          />
-        </Header>
+        <Paper elevation={3}>
+          <Header>
+            <SubHeader>
+              <Title>Bulichu Dos Bichus</Title>
+              <NavLinks />
+            </SubHeader>
+            <img
+              style={{ border: "solid 1px rgb(254, 210, 39)" }}
+              width={450}
+              src="/assets/pagina.png"
+            />
+          </Header>
+        </Paper>
         <div style={{ margin: 30 }}>
           <Routes>
             <Route exact path="/" element={<Home />} />
