5b8eb5edfb85e76e824d42e5dbcefbea2b064232
lucascincunegui <79164570+lucascincunegui@users.noreply.github.com>
2023-06-19 10:36:48 -0300

    BDB-32

--- src/components/list.js
+++ src/components/list.js
@@ -5,9 +5,9 @@ import { ItemsGrid, StyledBtn, PageNumber, StyledBtnGroup } from "./styles";
 export default function Lista({ products }) {
   const itemsPerPage = 8;
   const [items, setItems] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
   const totalPages = products.length / itemsPerPage;
-  console.log(products.length);
+  //NO PASA A LA ULTIMA PAGINA
   const setLista = () => {
     setItems([...products].splice(0, itemsPerPage));
   };
@@ -43,7 +43,7 @@ export default function Lista({ products }) {
   const prevHandler = () => {
     const prevPage = currentPage - 1;
 
-    if (prevPage < 1) return;
+    if (prevPage < 0) return;
 
     const firstIndex = prevPage * itemsPerPage;
 
@@ -53,13 +53,16 @@ export default function Lista({ products }) {
     window.scrollTo({ top: 300, behavior: "smooth" });
   };
 
+  console.log("TOTAL => " + totalPages);
+  console.log("CURRENT => " + currentPage);
+  console.log("PRODUCTS => " + products.length);
   return (
     <>
       <ItemsGrid container spacing={3}>
         {product}
       </ItemsGrid>
       <StyledBtnGroup orientation="horizontal">
-        {currentPage !== 1 ? (
+        {currentPage !== 0 ? (
           <StyledBtn onClick={prevHandler} variant="contained">
             Anterior
           </StyledBtn>
@@ -69,7 +72,7 @@ export default function Lista({ products }) {
           {currentPage} de {totalPages}
         </PageNumber>
 
-        {currentPage < totalPages && currentPage !== totalPages ? (
+        {currentPage <= totalPages ? (
           <StyledBtn onClick={nextHandler} variant="contained">
             Seguinte
           </StyledBtn>
