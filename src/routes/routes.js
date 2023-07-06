cdb6df56b9c4c991bb9cfff179b7ecc8d74c7e0a
lucascincunegui <79164570+lucascincunegui@users.noreply.github.com>
2023-07-06 17:48:43 -0300

    BDB-40

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Header, Title, Footer, Wrapper } from "./styles";
+import {
+  Header,
+  Title,
+  Footer,
+  Wrapper,
+  StyledBtn3,
+  RigthArrow,
+  TextFooter,
+} from "./styles";
 import NavLinks from "../components/navLinks";
 import Home from "../pages/home";
 import Products from "../pages/products";
 import Error404 from "../pages/error404";
 import { Paper } from "@material-ui/core";
+import { Facebook, Instagram, WhatsApp } from "@mui/icons-material";
 
 export default function MyRoutes() {
+  const facebook = () => {
+    window.open("https://www.facebook.com/bulichu.dosbichos.7");
+  };
+
+  const instagram = () => {
+    window.open("https://www.instagram.com/bulichudosbichos/");
+  };
+
   return (
     <Wrapper>
       <Router>
@@ -24,7 +41,17 @@ export default function MyRoutes() {
         </Routes>
         <Paper elevation={3}>
           <Footer>
-            <Title>Pie de pagina</Title>
+            <TextFooter>Siganos para entrar em contato</TextFooter>
+            <RigthArrow fontSize="large" />
+            <StyledBtn3 onClick={facebook}>
+              <Facebook fontSize="large" />
+            </StyledBtn3>
+            <StyledBtn3 onClick={instagram}>
+              <Instagram fontSize="large" />
+            </StyledBtn3>
+            <StyledBtn3>
+              <WhatsApp fontSize="large" />
+            </StyledBtn3>
           </Footer>
         </Paper>
       </Router>
